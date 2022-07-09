e33a238a822a7de49b143acdaada4c12d88468e2
Yung.Energy <morskojroma@gmail.com>
2022-07-09 15:23:13 +0300

    Task3

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int [] randomArray = new int [8];
+
+for(int index = 0; index < randomArray.Length; index++)
+{
+    randomArray[index] = new Random().Next(0,100);
+}
+PrintArray(randomArray);
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
